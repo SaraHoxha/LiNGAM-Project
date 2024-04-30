ddc221206f9a63551632ca8756c7f470c3b316ec
Alessandro Carella <alessandro.carella.universita@gmail.com>
2024-04-30 22:44:47 +0200

    added check for dag

--- algorithmImplementation/main.R
+++ algorithmImplementation/main.R
@@ -35,4 +35,57 @@ for (edge in edges) {
 cat (length(edges))
 
 #PLOT CAUSALITY GRAPH
-make_dot(res$Bpruned, colnames(data))
+#make_dot(res$Bpruned, colnames(data))
+
+
+# Function to perform topological sorting
+topological_sort <- function(graph) {
+  indegree <- numeric(length = length(graph))
+  for (i in seq_along(graph)) {
+    indegree[i] <- sum(graph[[i]] == TRUE)
+  }
+  
+  queue <- which(indegree == 0)
+  sorted <- vector("character", length = length(graph))
+  index <- 1
+  
+  while (length(queue) > 0) {
+    node <- queue[1]
+    queue <- queue[-1]
+    sorted[index] <- node
+    index <- index + 1
+    
+    neighbors <- which(graph[[node]] == TRUE)
+    for (neighbor in neighbors) {
+      indegree[neighbor] <- indegree[neighbor] - 1
+      if (indegree[neighbor] == 0) {
+        queue <- c(queue, neighbor)
+      }
+    }
+  }
+  
+  if (sum(indegree > 0) > 0) {
+    return(NULL) # Not a DAG
+  } else {
+    return(sorted)
+  }
+}
+
+# Construct adjacency matrix
+num_nodes <- ncol(matrix_data)
+adj_matrix <- matrix(0, nrow = num_nodes, ncol = num_nodes)
+for (edge in edges) {
+  from_index <- which(colnames(data) == edge$from)
+  to_index <- which(colnames(data) == edge$to)
+  adj_matrix[to_index, from_index] <- 1
+}
+
+# Perform topological sorting
+sorted_nodes <- topological_sort(graph = adj_matrix)
+
+# Check if the graph is a DAG
+if (is.null(sorted_nodes)) {
+  cat("The graph is not a Directed Acyclic Graph (DAG).\n")
+} else {
+  cat("The graph is a Directed Acyclic Graph (DAG).\n")
+}
--- x.py
+++ x.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("statsDfNormalized.csv")
 
-df.drop(columns=["valence","tempo","n_beats","n_bars"], axis=1)
+
+#speechiness,acousticness,instrumentalness,liveness,tempo,n_beats,n_bars
+"danceability", "liveness", "energy", "acousticness", "loudness"
+df = df.drop(columns=["speechiness","instrumentalness","valence","tempo","n_beats","n_bars"], axis=1)
 
 df.to_csv("statsDfNormalizedWithLessColumns.csv", index=False)
