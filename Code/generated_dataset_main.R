124b265c58e29aa1f650ea3207bfc208cff5e1ae
milica <militsa.rad@gmail.com>
2024-07-21 19:49:58 +0200

    generated dataset changes

--- Code/generated_dataset_main.R
+++ Code/generated_dataset_main.R
@@ -3,38 +3,27 @@ library(igraph)
 source("utils.R")
 
 set.seed(123)
-
 # Load the dataset
-happiness_data <- read_dataframe("generated_data.csv")
+happiness_data <- read_dataframe("C:/Users/milit/OneDrive/Desktop/stats_project/Statistics-for-data-science/Dataset/generated_data.csv")
 
 happiness_result <- lingam_algorithm(happiness_data)
 
 #BEFORE PRUNING
-print("B matrix BEFORE:")
+print("B matrix :")
 print(happiness_result$B)
 print("Causal Order")
 print(happiness_result$causal_order)
-print("Adjacency matrix BEFORE:")
+print("Adjacency matrix :")
 adjacency_matrix <- happiness_result$adjacency_matrix
 print(happiness_result$adjacency_matrix)
-plot_causality_graph(happiness_result$adjacency_matrix,colnames(happiness_data), "Happiness BEFORE PRUNING")
+plot_causality_graph(happiness_result$adjacency_matrix,colnames(happiness_data), happiness_result$causal_order, "Happiness")
 print(is_dag(adjacency_matrix))
+print("Wald test result")
+print(happiness_result$wald_test_results)
 
 is_dag <- function(adjacency_matrix) {
   graph <- graph.adjacency(adjacency_matrix, mode = "directed")
   return(is.dag(graph))
 }
-
-#AFTER PRUNING
-#pruning_result = wald_test(happiness_result$B)
-
-#print("B matrix AFTER:")
-#print(happiness_result$B_pruned)
-
-#print("Adjacency matrix AFTER:")
-#pruning_result$adjacency_matrix_pruned
-
-#plot_causality_graph(pruning_result$adjacency_matrix_pruned,colnames(happiness_data), "Happiness AFTER PRUNING")
-#print(is_dag(pruning_result$adjacency_matrix_pruned))
 
 
